f65ba595ddc8b3b2bb5abbc641da4fa426bd2aa9
janburian <jburian@students.zcu.cz>
2023-01-10 09:56:50 +0100

    Loading qupath file

---
+++ devel/qupath.py
@@ -0,0 +1,15 @@
+import javaobj
+
+def read_file(filename:str):
+    file = open(filename, "rb")
+    data = file.read()
+    file.close()
+    return data
+
+if __name__ == '__main__':
+    filename = "data.qpdata"
+    jobj = read_file(filename)
+    pobj = javaobj.loads(jobj)
+    test = jobj.decode("utf-8")
+    print(pobj)
+
